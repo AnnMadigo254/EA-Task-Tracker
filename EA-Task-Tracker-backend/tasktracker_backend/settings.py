5e8a5701e67d23cfc43b25f3494016bdbedc7d78
AnnMadigo254 <amadigo@co-opbank.co.ke>
2026-01-29 15:11:43 +0300

    fixed port issues

--- EA-Task-Tracker-backend/tasktracker_backend/settings.py
+++ EA-Task-Tracker-backend/tasktracker_backend/settings.py
@@ -193,10 +193,7 @@ SIMPLE_JWT = {
 # CORS Configuration
 # CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='http://localhost:5173,http://localhost:3000').split(',')
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 
